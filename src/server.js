e2c34355f8296ebf475b2dc36207bc5edad55533
Ivan Kabar <alkazavr94@google.com>
2023-03-29 14:46:29 +0200

    new

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/route1', function(req, res) {
+    res.send('This is Route 1');
+});
+
+
+app.get('/route2', function(req, res) {
+    res.send('This is Route 2');
+});
+
+app.get('/route3', function(req, res) {
+    res.send('This is Route 3');
+});
+
+app.listen(3000, function() {
+    console.log('Server started on port 3000');
+});

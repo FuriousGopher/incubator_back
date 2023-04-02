ccba6aa88928cc161eab047705c50c1b8d574125
Ivan Kabar <alkazavr94@google.com>
2023-04-02 16:39:02 +0200

    changes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,8 @@ type videosType = {
     availableResolutions?: resolutions[] | null;
 }
 
+const dataRegex = /^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9])\.[0-9]{3}Z$/
+
 const validateAvailableResolution = (resolution: unknown[]): boolean => {
     for (let i = 0; i < resolution.length; i++){
         // @ts-ignore
@@ -73,6 +75,9 @@ const validateBody = ({
         errorsMessages.push({message: 'Error', field: 'minAgeRestriction'})
     }
 
+    if (publicationDate?.match(dataRegex)) {
+        errorsMessages.push({message: 'Error', field: 'minAgeRestriction'})
+    }
 
     if (errorsMessages.length > 0) {
         return {errorsMessages}

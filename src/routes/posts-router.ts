a2ac7f6e8cefbda9d36d557cb7ad371ccd9d5209
Ivan Kabar <alkazavr94@gmail.com>
2023-04-10 14:30:41 +0200

    try to catch error

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,12 +1,19 @@
 import {Router} from "express";
-import {createNewPost, deletePostsById, getAllPosts, getPostsById, updatePostById} from "../controllers/postsController";
+import {
+    createNewPost,
+    deletePostsById,
+    getAllPosts,
+    getPostsById,
+    updatePostById
+} from "../controllers/postsController";
 import {loginValidationRules, validateLogin} from "../middlewares/authorization";
 import {validatePostAndPutMethodsForPostsBody} from "../validators/postValidator";
 import {validationMiddleware} from "../validators/blogsValidator";
+
 export const postsRouter = Router()
 
 postsRouter.get('/', getAllPosts)
 postsRouter.get('/:id', getPostsById);
-postsRouter.post('/', validatePostAndPutMethodsForPostsBody,validationMiddleware, validateLogin, loginValidationRules,  createNewPost);
-postsRouter.delete('/:id', loginValidationRules,validateLogin, deletePostsById);
-postsRouter.put('/:id', validatePostAndPutMethodsForPostsBody,validationMiddleware, validateLogin, loginValidationRules,  updatePostById );
+postsRouter.post('/', loginValidationRules, validateLogin, validatePostAndPutMethodsForPostsBody, validationMiddleware,  createNewPost);
+postsRouter.delete('/:id', loginValidationRules, validateLogin, deletePostsById);
+postsRouter.put('/:id', loginValidationRules, validateLogin, validatePostAndPutMethodsForPostsBody, validationMiddleware, updatePostById);

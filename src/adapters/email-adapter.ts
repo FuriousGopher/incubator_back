48dc3854d897751b10612316f59b2db0918c175f
Ivan Kabar <alkazavr94@gmail.com>
2023-05-06 16:17:28 +0200

    HW_7

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -4,20 +4,20 @@ import { settings } from '../settings';
 export const emailAdapter = {
   async sendEmail(email: string, confirmationCode: string) {
     const transport = nodemailer.createTransport({
-      service: 'gmail',
+      service: "gmail",
       auth: {
         user: settings.MY_EMAIL,
-        pass: settings.EMAIL_PASS,
-      },
+        pass: settings.EMAIL_PASS
+      }
     });
     return await transport.sendMail({
-      from: 'Ricky',
+      from: "Ricky",
       to: email,
-      subject: 'BackEndIncubatorTest',
+      subject: "BackEndIncubatorTest",
       html: `<h1>Thank for your registration</h1>
       <p>To finish registration please follow the link below:
-      <a href="https://705c-77-71-152-160.ngrok-free.app/auth/registration-confirmation?code=${confirmationCode}">complete registration</a>
-      </p>`,
+      <a href="https://incubator-back.vercel.app/auth/registration-confirmation?code=${confirmationCode}">complete registration</a>
+      </p>`
     });
-  },
+  }
 };

6cf4cc6f8292c8bc064a31b4e0f416053a1c898c
Ivan Kabar <alkazavr94@gmail.com>
2023-06-26 17:01:07 +0200

    added inversify fixing

--- src/composition-root.ts
+++ src/composition-root.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import { UsersService } from './services/usersService';
 import { UsersRepositories } from './repositories/users-repositories';
 import { UsersController } from './controllers/usersController';
-import { Container } from 'added inversify';
+import { Container } from 'inversify';
 export const container = new Container();
 container.bind(UsersController).to(UsersController);
 container.bind(UsersService).to(UsersService);
